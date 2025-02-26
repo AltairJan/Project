d9a2d77c6a6a95b995fcb598b564a01491d1a4d6
AltairJan <126330094+AltairJan@users.noreply.github.com>
2025-02-26 10:56:11 +0100

    Opravy nazvu složek a spuštění simulace Simulink

---
+++ Robot_DUM-E/V0.3/Overeni_Matlab/SimScape_Model/Mat_code_varables_.m
@@ -0,0 +1,29 @@
+clc 
+close all
+clear 
+
+% smimport("Model/_Robot_Arm.xml")
+run("Robot_Arm_DataFile.m");
+
+%% Parametry
+d0 = 0.1207;
+a1 = 0.265;
+a2 = 0.22313448854;
+a3 = 0.0995;
+d4 = 0.1355;
+d5 = 0.200;
+
+%% parameters for model
+
+g3 = 211.6244444;
+T3 = [cosd(g3) -sind(g3) 0;
+      sind(g3)  cosd(g3) 0;
+      0         0        1];
+
+g4 = 180;  
+T4 = [1 0           0
+      0 cosd(g4)    -sind(g4)
+      0 sind(g4)    cosd(g4)];
+  
+%%
+Data = sim('SimScape_Simulation.slx');
